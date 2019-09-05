90bdde00b05223d4ebdc9fecfeaa3469783558ff
Roneet Kumar <roneetparida@gmail.com>
2019-09-05 16:17:08 -0400

    update

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
 import os
 
+
 def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
 
+
 def divide(x, y):
     return x / y
+
 
 print("Select operation.")
 print("1.Add")
@@ -24,16 +29,16 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
+    print(f"{num1} + {num2} = {add(num1, num2)}")
 
 elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
 
 elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
 
 elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
 else:
     print("Invalid input")
 
